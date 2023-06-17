26cb1e260e03662c8ccfe54a3ece996046c15f31
Allison Tapia <tapioca204@hotmail.com>
2023-06-17 12:59:15 -0700

    Creando class Nodo

---
+++ ListasLigadas/Nodo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasLigadas
+{
+    internal class Nodo
+    {
+        public Nodo(string valor = "", Nodo enlace = null)
+        {
+            Valor = valor;
+            Enlace = enlace;
+        }
+
+        public string Valor { get; set; }
+        public Nodo Enlace { get; set; }
+
+        
+    }
+}
